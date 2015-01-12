56bbcea33938b0e70ea2a8c9ebfca8459f988e76
Garrett Amini <kadjar@gmail.com>
2015-01-12 12:15:22 -0800

    version++

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule OAuth2Cli.Mixfile do
   def project do
     [
       app: :oauth2cli,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.0",
       deps: deps,
       description: @description,
